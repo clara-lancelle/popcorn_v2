bcab140f698f43698ab59e4e0baae2b770ef87f0
pinklady <lancelle.clara@hotmail.com>
2025-12-18 15:06:25 +0100

    repare vite navbar

--- src/Components/layouts/Navbar.jsx
+++ src/Components/layouts/Navbar.jsx
@@ -1,58 +1,38 @@
 import { NavLink, Link } from "react-router-dom";
 import useDebounce from "../../useDebounce";
 import HandleSearch from "../HandleSearch";
+import { Navbar as Nav, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
 
 function Navbar() {
     const [search, setSearch] = useDebounce(1000);
 
     return (
-        <nav className="border-gray-200 bg-gray-50">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Nav className="border-gray-200 bg-gray-50">
+                <NavbarBrand as={Link} href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <span className="self-center text-2xl font-semibold whitespace-nowrap ">🍿 PopCorn</span>
-                </Link>
-                <div className="relative hidden md:block">
-                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg className="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                        <span className="sr-only">Search icon</span>
-                    </div>
-                    <input type="search" onInput={(e) => setSearch(e.target.value.replace(/ /g, '+'))} id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search..." />
-                </div>
-                <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-solid-bg" aria-expanded="false">
-                    <span className="sr-only">Open main menu</span>
-                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-                    <div className="relative mt-3 md:hidden">
+                </NavbarBrand>
+                <NavbarToggle />
+                <NavbarCollapse>
+                    <div className="relative md:block">
                         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <svg className="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
+                            <span className="sr-only">Search icon</span>
                         </div>
-                        <input type="search" onInput={(e) => setSearch(e.target.value.replace(/ /g, '+'))} id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search..." />
+                        <input type="search" onInput={(e) => setSearch(e.target.value.replace(/ /g, '+'))} id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search..." />
                     </div>
-                    <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
-                        <li>
-                            <NavLink to="/fav" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">
-                                Favorites
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/rated" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">
-                                Rated movies
-                            </NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+                    <NavbarLink as={NavLink} href="/fav" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">
+                        Favorites
+                    </NavbarLink>
+                    <NavLink to="/rated" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">
+                        Rated movies
+                    </NavLink>
+                </NavbarCollapse>
             {search &&
                 <HandleSearch search={search} />
             }
-        </nav >
+        </Nav >
     );
 }
 
