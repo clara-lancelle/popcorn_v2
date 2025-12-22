bd8a8c91795f1428b5e448a82b239cf2f701e797
pinklady <lancelle.clara@hotmail.com>
2025-12-22 10:35:48 +0100

    repare link footer

--- src/Components/layouts/AppFooter.jsx
+++ src/Components/layouts/AppFooter.jsx
@@ -1,17 +1,17 @@
+import { Link } from "react-router-dom";
 import {
     Footer,
     FooterCopyright,
     FooterDivider,
-    FooterLink,
     FooterLinkGroup,
     FooterTitle,
 } from "flowbite-react";
 
 export default function AppFooter({ onCookiesClick }) {
     return (
-        <Footer container className="border-gray-200 bg-gray-50">
+        <Footer container className="border-gray-200 bg-gray-50 mt-2">
             <div className="w-full">
-                <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-2">
+                <div className="mx-auto w-full max-w-screen-xl p-4 py-1">
                     <div className="md:flex md:justify-between">
                         <div className="mb-6 md:mb-0">
                             <span className="flex mr-4 items-center self-center text-2xl font-semibold whitespace-nowrap ">
@@ -22,34 +22,30 @@ export default function AppFooter({ onCookiesClick }) {
                             <div>
                                 <FooterTitle title="About" />
                                 <FooterLinkGroup>
-                                    <FooterLink href="/about">
-                                        About Me{" "}
-                                    </FooterLink>
+                                    <Link to="/about">About Me </Link>
                                 </FooterLinkGroup>
                             </div>
                             <div>
                                 <FooterTitle title="Legal" />
                                 <FooterLinkGroup col>
-                                    <FooterLink href="/legal-notice">
-                                        Legal Notice
-                                    </FooterLink>
-                                    <FooterLink href="/privacy-policy">
+                                    <Link to="/legal-notice">Legal Notice</Link>
+                                    <Link to="/privacy-policy">
                                         Privacy Policy
-                                    </FooterLink>
-                                    <FooterLink href="/cookies-policy">
+                                    </Link>
+                                    <Link to="/cookies-policy">
                                         Cookies Policy
-                                    </FooterLink>
+                                    </Link>
                                 </FooterLinkGroup>
                             </div>
                             <div>
                                 <FooterTitle title="Action" />
                                 <FooterLinkGroup>
-                                    <FooterLink
+                                    <Link
                                         className="hover:cursor-pointer"
                                         onClick={onCookiesClick}
                                     >
                                         Update Cookies Preferences
-                                    </FooterLink>
+                                    </Link>
                                 </FooterLinkGroup>
                             </div>
                         </div>
@@ -57,7 +53,7 @@ export default function AppFooter({ onCookiesClick }) {
                 </div>
                 <FooterDivider />
                 <FooterCopyright
-                    href="#"
+                    to="#"
                     by="Lancelle Clara"
                     year={new Date().getFullYear()}
                 />
--- src/Components/pages/About.jsx
+++ src/Components/pages/About.jsx
@@ -4,39 +4,48 @@ import { FaReact } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function About() {
-  return (
-    <main className="max-w-4xl mx-auto p-6 space-y-6">
-      <h1 className="text-3xl font-bold">About Me</h1>
+    return (
+        <main className="max-w-4xl mx-auto p-6 space-y-6">
+            <h1 className="text-3xl font-bold">About Me</h1>
 
-      <section>
-        <img
-          alt="Lancelle Clara Photo/Image"
-          src="/public/Lancelle_clara.jpg"
-          className="mb-5 rounded-full shadow-lg h-44 justify-self-center md:justify-self-auto"
-        />
-        <h2 className="text-xl font-semibold">Hi, my name is Clara</h2>
-        <p> I'm a web developer who lives in France, Nantes (44000).</p>
-        <p className="text-sm text-gray-600 mt-2">
-          This website is part of my portfolio, to see more of it check out my
-          other websites, my Github repositories or my Linkedin page.
-        </p>
-      </section>
-      <section>
-        <List>
-          <Link to={`https://github.com/clara-lancelle`} target="_blank">
-            <ListItem icon={BsGithub}>My Github</ListItem>
-          </Link>
-          <Link
-            to={`https://www.linkedin.com/in/clara-lancelle-159634238/`}
-            target="_blank"
-          >
-            <ListItem icon={BsLinkedin}>My Linkedin</ListItem>
-          </Link>
-          <Link to={`https://password-generator.lncl.fr/`} target="_blank">
-            <ListItem icon={FaReact}>My Password generator website 🔑</ListItem>
-          </Link>
-        </List>
-      </section>
-    </main>
-  );
+            <section>
+                <img
+                    alt="Lancelle Clara Photo/Image"
+                    src="/images/Lancelle_clara.jpg"
+                    className="mb-5 rounded-full shadow-lg h-44 justify-self-center md:justify-self-auto"
+                />
+                <h2 className="text-xl font-semibold">Hi, my name is Clara</h2>
+                <p> I'm a web developer who lives in France, Nantes (44000).</p>
+                <p className="text-sm text-gray-600 mt-2">
+                    This website is part of my portfolio, to see more of it
+                    check out my other websites, my Github repositories or my
+                    Linkedin page.
+                </p>
+            </section>
+            <section>
+                <List>
+                    <Link
+                        to={`https://github.com/clara-lancelle`}
+                        target="_blank"
+                    >
+                        <ListItem icon={BsGithub}>My Github</ListItem>
+                    </Link>
+                    <Link
+                        to={`https://www.linkedin.com/in/clara-lancelle-159634238/`}
+                        target="_blank"
+                    >
+                        <ListItem icon={BsLinkedin}>My Linkedin</ListItem>
+                    </Link>
+                    <Link
+                        to={`https://password-generator.lncl.fr/`}
+                        target="_blank"
+                    >
+                        <ListItem icon={FaReact}>
+                            My Password generator website 🔑
+                        </ListItem>
+                    </Link>
+                </List>
+            </section>
+        </main>
+    );
 }
